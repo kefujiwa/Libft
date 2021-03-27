7a76e2edb1d4eede7603dbfd6a07c895d83a34d0
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-27 16:18:11 +0900

    fix(ft_dlstadd_front.c): fix segmentation fault error; add condition when *lst is NULL
    - セグフォ解消; *lstが存在するときのみ、(*lst)->prevにアクセスするように修正

--- srcs/dlist/ft_dlstadd_front.c
+++ srcs/dlist/ft_dlstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 15:07:17 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/03/27 15:41:26 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/03/27 16:07:08 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_dlstadd_front(t_dlist **lst, t_dlist *new)
 	if (!lst || !new)
 		return ;
 	new->next = *lst;
-	(*lst)->prev = new;
+	if (*lst)
+		(*lst)->prev = new;
 	*lst = new;
 }
