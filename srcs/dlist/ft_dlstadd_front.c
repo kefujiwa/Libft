99f25dc9f3130d5ae2e9a443a368847840039477
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-27 15:43:41 +0900

    fix(fd_dlstadd_front.c): add updating prev process
    - 要素を先頭に追加する際に、直前まで先頭にあった要素のprevメンバに新しく先頭となる要素のポインタを代入する処理を追加

--- srcs/dlist/ft_dlstadd_front.c
+++ srcs/dlist/ft_dlstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 15:07:17 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/03/27 15:09:38 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/03/27 15:41:26 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ void	ft_dlstadd_front(t_dlist **lst, t_dlist *new)
 	if (!lst || !new)
 		return ;
 	new->next = *lst;
+	(*lst)->prev = new;
 	*lst = new;
 }
