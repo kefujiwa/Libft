187635542237170db600b0634df342e5b74bbd15
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-28 17:05:13 +0900

    style(ft_strcmp.c): fix indentation for norminette

--- srcs/str/ft_strcmp.c
+++ srcs/str/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 01:07:56 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/03/24 01:13:51 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/03/28 17:04:09 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	ft_strcmp(const char *s1, const char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
 	{
-			s1++;
-			s2++;
+		s1++;
+		s2++;
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
